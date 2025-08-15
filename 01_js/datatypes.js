8e59a122be6f1f7d9e0e11fd502212dca8a2f1fc
Ashish1h <ashishrajkgg2005@gmail.com>
2025-08-15 20:39:08 +0000

    commit

---
+++ 01_js/datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version 
+
+//  alert(3+3)  // we are using nodejs , not browser
+
+/*
+number => 2 to the power 23
+bigInt
+string => ""
+boolean => true/flase
+null => standalone value
+undefined =>
+symbol => unique
+*/
+
+let name = "jasnj"
+let ashish = 99
+let state;
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);  // object
+
+
--- 01_js/text.js
+++ 01_js/text.js
@@ -1,1 +1,18 @@
-console.log("Ashish");
+const accountId = 144625
+let accountEmail = "ashishydv@gmail.com"
+var accountPassword = "72690"
+accountCity = "Noida" 
+
+
+//accountId = 24  // not allowed
+
+accountEmail = "h1sh@google.com"
+accountPassword = "7677236"
+accountCity = "hyderabad"
+
+/*
+don't use var
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity,])
+
