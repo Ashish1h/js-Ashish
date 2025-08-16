a6d1b486e47c6f7b560942b445ac7ace5f80fec7
Ashish1h <ashishrajkgg2005@gmail.com>
2025-08-16 06:01:28 +0000

    conversion of datatype

---
+++ 01_js/conversion.js
@@ -0,0 +1,25 @@
+let score = "45"
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof (valueInNumber));
+console.log(valueInNumber)
+
+// "45" => 45
+// "45abc" => NaN
+// ture => 1;  false => 0
+
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Ashish" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof (stringNumber));
